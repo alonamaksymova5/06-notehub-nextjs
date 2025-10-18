dd0bf99463293f00e76aab3e8554631a5ce1863b
Alona Maksymova <alonamaksymova5@gmail.com>
2025-10-18 15:24:59 +0300

    start project

---
+++ lib/api.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+import type { NewNote, Note } from "../types/note";
+
+interface NotesResponse {
+  notes: Note[];
+  totalPages: number;
+}
+
+axios.defaults.baseURL = "https://notehub-public.goit.study/api";
+
+export async function fetchNotes(
+  page: number,
+  perPage: number,
+  search: string
+): Promise<NotesResponse> {
+  const res = await axios.get<NotesResponse>("/notes", {
+    params: { page, perPage, search },
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`,
+    },
+  });
+  return res.data;
+}
+
+export async function createNote(newNote: NewNote): Promise<Note> {
+  const res = await axios.post("/notes", newNote, {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`,
+    },
+  });
+  return res.data;
+}
+
+export async function deleteNote(noteId: string): Promise<Note> {
+  const res = await axios.delete<Note>(`/notes/${noteId}`, {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`,
+    },
+  });
+  return res.data;
+}
