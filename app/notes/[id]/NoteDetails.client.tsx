adc22dbcbe5dcaa068b2ecc6101be27a852bb9f5
Alona Maksymova <alonamaksymova5@gmail.com>
2025-10-27 21:16:58 +0200

    add refetch

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -18,6 +18,7 @@ export default function NoteDetailsPageClient({ id }: { id?: string }) {
     queryKey: ["note", noteId],
     queryFn: () => fetchNoteById(noteId),
     enabled: !!noteId,
+    refetchOnMount: false,
   });
 
   if (isLoading) {
